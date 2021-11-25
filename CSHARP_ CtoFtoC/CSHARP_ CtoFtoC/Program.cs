e58b9579309504728393e7863b48dc9e86bbf9c8
dntn1009 <67273308+dntn1009@users.noreply.github.com>
2021-11-25 22:25:25 +0900

    CSHARP_CtoFtoC
    CSHARP_CtoFtoC

---
+++ CSHARP_ CtoFtoC/CSHARP_ CtoFtoC/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSHARP__CtoFtoC
+{
+    // C++과 다른 적용법
+    // class 사용법
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int select = 0;
+            float temp = 0;
+            Console.Write("1. 섭씨->화씨\n");
+            Console.Write("2. 화씨->섭씨\n");
+            select = int.Parse(Console.ReadLine());
+            switch (select)
+            {
+                case 1:
+                    Console.Write("변환할 섭씨 온도를 입력해주세요.\n");
+                    temp = float.Parse(Console.ReadLine());
+                    temp = Temperature.CtoF(temp);
+                    Console.Write("변환한 온도는 화씨 {0:f2} 입니다. \n", temp);
+                    break;
+                case 2:
+                    Console.Write("변환할 화씨 온도를 입력해주세요.\n");
+                    temp = float.Parse(Console.ReadLine());
+                    temp = Temperature.FtoC(temp);
+                    Console.Write("변환한 온도는 섭씨 {0:f2} 입니다. \n", temp);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ CSHARP_ CtoFtoC/CSHARP_ CtoFtoC/Temperature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Temperature
+{
+    public static float CtoF(float temp)
+    {
+        return (temp * 9 / 5) + 32;
+    }
+    public static float FtoC(float temp)
+    {
+        return (temp - 32) * 5 / 9;
+    }
+}
